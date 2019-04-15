375b008a510ea263744c22d61fd9c85037a92a6c
Flo <floxyao@gmail.com>
2019-04-15 12:41:37 -0700

    initial commit

---
+++ struct.h
@@ -0,0 +1,16 @@
+//===============================================================================================
+//HW 4:     P2P
+//Author:   Florence Yao
+//Course:   CECS-327
+//Date:     4-10-19
+//Info: 
+//===============================================================================================
+#include <string.h>
+
+struct ServantData{
+    uint32_t GUID;
+    //int last_update;
+    //const char *my_file[50];
+    const char my_file[50];
+};
+//#pragma pack(0)
