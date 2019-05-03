a457939a5b35d9b20d8fb9dd8137977278b73dd0
Flo <floxyao@gmail.com>
2019-05-03 10:10:52 -0700

    udp + user input done

--- server.c
+++ server.c
@@ -13,23 +13,17 @@
 #include <string.h>
 #include <fcntl.h>
 #include <pthread.h>
-#include "struct.h"
+#include "helper.h"
+
 #define SENDER_NAME "server: "
 #define PORT 8080
 #define NUM_THREADS 2
 #define MSG_LEN 100
 #define BUF_SIZE 1024
 
-
-struct Registry{
-    struct ServantData servants[2];
-    int size;
-} reg;
-//================================================================================
-// SOCKET STUFF
-//================================================================================
 int       server_fd, udp_fd, new_socket, new_socket2;
 struct    sockaddr_in server_address, client_address;
+struct ServantData rcv_data;
 int       addrlen = sizeof(server_address);
 char      msg[MSG_LEN];
 char      buffer[BUF_SIZE] = {0};
@@ -37,61 +31,154 @@ ssize_t   n;
 socklen_t len; 
 
 //================================================================================
-// Concat two strings 
-// "server:  " + <message>
-// "client1: " + <message>
+// Function: tcp_thread
+// Handles TCP socket connection/communication with TCP client
+// This is run in one of the multithreads alongside UDP thread
 //================================================================================
-void concat(char* p, char *q){
-   int c, d;
-   c = 0;
-   while (p[c] != '\0') {
-      c++;      
-   }
-   d = 0;
-   while (q[d] != '\0') {
-      p[c] = q[d];
-      d++;
-      c++;    
-   }
-   p[c] = '\0';
-}
+void* tcp_thread(void* arg){
 
-//================================================================================
-// Function: publish
-// Purpose: outputs all client GUID's and 
-//          files that each client has
-//================================================================================
-void publish(int client_no){
-    // printf("\nPrinting Servants lists of files:");
-    printf("\n====================================\n");
-    printf("Client 1 GUID: %d",   reg.servants[client_no].GUID);
-    printf("\nClient 1 files %s\n", reg.servants[client_no].my_file);
-    // printf("\nClient 2 GUID: %d",   reg.servants[1].GUID);
-    // printf("\nClient 2 files %s", reg.servants[1].my_file);
-    printf("====================================\n");
+    /*-------------------
+     Creating TCP socket 
+    ---------------------*/
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{ 
+		perror("TCP socket failed"); 
+		exit(EXIT_FAILURE); 
+    }
+    else{
+        printf("\nTCP Socket Created!\n");
+    }
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY; 
+	server_address.sin_port = htons( PORT ); 
+	
+	/*-------------------------------------------
+      TCP Bind - 
+      once you have a socket, bind
+      it to a port on your machine
+    --------------------------------------------*/
+	if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address))<0)
+	{ 
+		perror("tcp bind failed"); 
+		exit(EXIT_FAILURE); 
+    }
+
+    /*-----------------------------
+      TCP Listen - 
+      original socket server_fd 
+      listens for more incoming 
+      connections
+    ------------------------------*/
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen error"); 
+		exit(EXIT_FAILURE); 
+	}
+    else{
+        printf("\nTCP Listening...\n");
+    }
+
+    /*------------------------------------------------------------------------------
+     Accept client 1 connection
+    -------------------------------------------------------------------------------*/
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&server_address, 
+					(socklen_t*)&addrlen)) < 0)
+	{ 
+		perror("accept error 1\n"); 
+		exit(EXIT_FAILURE); 
+	}
+    else{
+        /*---------------------
+            Register Client
+        ----------------------*/
+        printf("\nTCP Socket 1 Connected!\n");
+        struct ServantData rcv_data;
+        time_t t = time(NULL);           
+
+        recv(new_socket , &rcv_data, sizeof(rcv_data), 0);        // get obj
+
+        rcv_data.GUID = reg.size+1;                               // GUID = size of reg cause unique
+        rcv_data.time = gmtime(&t);                               // set time stamp
+        rcv_data.alive = TRUE;                                    // mark this client alive
+        reg.servants[reg.size++] = rcv_data;
+        print(rcv_data.GUID-1);                                    // print client information
+
+        int n = send(new_socket, &rcv_data, sizeof(rcv_data), 0); // send back GUID
+    }
+
+    /*------------------------------------------------------------------------------
+     Accept client 1 connection
+    -------------------------------------------------------------------------------*/
+    if ((new_socket2 = accept(server_fd, (struct sockaddr *)&server_address, 
+					(socklen_t*)&addrlen)) < 0)
+	{ 
+		perror("accept error 2\n"); 
+		exit(EXIT_FAILURE); 
+	}
+    else{
+        /*---------------------
+            Register Client
+        ----------------------*/
+        printf("\nTCP Socket 2 Connected!\n");
+        //struct ServantData rcv_data;
+        time_t t = time(NULL);
+
+        recv(new_socket2 , &rcv_data, sizeof(rcv_data), 0);        // get obj
+
+        rcv_data.GUID = reg.size+1;                                // GUID = size of reg cause unique
+        rcv_data.time = gmtime(&t);                                // set time stamp
+        rcv_data.alive = TRUE;                                     // mark this client alive
+        
+        reg.servants[reg.size++] = rcv_data;
+        print(rcv_data.GUID-1);                                    // print client information
+  
+        int m = send(new_socket2, &rcv_data, sizeof(rcv_data), 0); // send obj
+    }
+
+    /*-----------------------------------
+     Change socket to non-blocking
+     needs to happen after registration
+    -----------------------------------*/
+    fcntl(new_socket2, F_SETFL, O_NONBLOCK);
+    fcntl(new_socket, F_SETFL, O_NONBLOCK);
+    fcntl(server_fd, F_SETFL, O_NONBLOCK);
+
+    for(;;){
+        //printf("\ntcp .\n");
+        int valread = recv( new_socket , buffer, BUF_SIZE, 0);
+        int valread2 = recv( new_socket2 , buffer, BUF_SIZE, 0);
+
+        // send(new_socket , msg , strlen(msg) , 0);
+        // send(new_socket2 , msg , strlen(msg) , 0);
+        // memset(msg, 0, MSG_LEN);
+        //printf("\nTCP BUFFER\n");
+        printf("%s", buffer);                                       //display message
+        bzero(buffer, sizeof(buffer));                              //flush buffer
+        sleep(3);                                                   //introduce delay or else loops too fast (?)                                  
+    }
+
+    pthread_exit(NULL);
 }
 
 //================================================================================
 // Function: udp_thread
 // Handles UDP connection/communication with UDP client
 // Send "Datagrams" over network to notify client is still connected
+// This is run in one of the multithreads along with TCP thread
 //================================================================================
 void* udp_thread(void* arg){
+    
     char *hello = "Hello from server"; 
 
     /*-----------------------------------------------------
      Creating UDP socket 
     ------------------------------------------------------*/
     if ( (udp_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("                    socket creation failed"); 
+        perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } else{
-        printf("\n                    UDP Socket Created!\n"); 
+        printf("\nUDP Socket Created!\n"); 
     }
-    // struct timeval read_timeout;
-    // read_timeout.tv_sec = 0;
-    // read_timeout.tv_usec = 10;
-    // setsockopt(udp_fd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
 
     memset(&server_address, 0, sizeof(server_address)); 
     memset(&client_address, 0, sizeof(client_address)); 
@@ -108,167 +195,28 @@ void* udp_thread(void* arg){
     ------------------------------------------------------*/
     if ( bind(udp_fd, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0 ) 
     { 
-        perror("                    bind failed"); 
+        perror("udp bind failed"); 
         exit(EXIT_FAILURE); 
     }
 
     /*-----------------------------------------------------
-     Wait for message from UDP client
+      Wait for ping from UDP client
     ------------------------------------------------------*/
     for(;;){
         n = recvfrom(udp_fd, (char *)buffer, BUF_SIZE, MSG_WAITALL, (struct sockaddr *) &client_address, &len); 
         buffer[n] = '\0'; 
-        printf("                    UDP Client: %s\n", buffer); 
+        printf("Ping from Client %s @ ", buffer); 
+    
+        // check who sent the UDP message
+        int CLIENT_NO = buffer[0] - '0';                        // convert ASCII to int  
+        printf("client id = %d", CLIENT_NO);
+
+        check_clients_if_alive_or_dead(CLIENT_NO);
 
         bzero(buffer, sizeof(buffer));
-        sleep(3);
+        sleep(1);
     }
     close(udp_fd); 
-    pthread_exit(NULL);
-}
-
-//================================================================================
-// Func: tcp_thread
-// Handles TCP socket connection/communication with TCP client
-//================================================================================
-void* tcp_thread(void* arg){
-
-    /*--------------------------------------------------------------------------
-     Creating TCP socket 
-    ---------------------------------------------------------------------------*/
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{ 
-		perror("TCP socket failed"); 
-		exit(EXIT_FAILURE); 
-    }
-    else{
-        printf("\nTCP Socket Created!\n");
-    }
-	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = INADDR_ANY; 
-	server_address.sin_port = htons( PORT ); 
-	
-	/*--------------------------------------------------------------------------
-      TCP Bind - once you have a socket, bind() it to a port on your machine
-    ---------------------------------------------------------------------------*/
-	if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address))<0)
-	{ 
-		perror("tcp bind failed"); 
-		exit(EXIT_FAILURE); 
-    }
-
-    /*-------------------------------------------------------------------------
-      TCP Listen - original socket server_fd listens for more incoming connections
-    -------------------------------------------------------------------------*/
-	if (listen(server_fd, 3) < 0) 
-	{ 
-		perror("listen error"); 
-		exit(EXIT_FAILURE); 
-	}
-    else{
-        printf("\nTCP Listening...\n");
-    }
-
-    /*---------------------------------
-     Accept client 1 connection
-    ----------------------------------*/
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&server_address, 
-					(socklen_t*)&addrlen)) < 0)
-	{ 
-		perror("accept error 1\n"); 
-		exit(EXIT_FAILURE); 
-	}
-    else{
-        printf("\nTCP Socket 1 Connected!\n");
-
-        /*---------------------------------
-           JOIN & PUBLISH
-        ----------------------------------*/
-        struct ServantData rcv_data;
-
-        // GET OBJ. FROM CLIENT 1 
-        recv(new_socket , &rcv_data, sizeof(rcv_data), 0);
-        rcv_data.GUID = reg.size+1;
-
-        // JOIN
-        reg.servants[reg.size] = rcv_data;
-
-        // PUBLISH
-        publish(reg.size); // reg.size (size of registry) used for unique GUID
-        
-        reg.size++;
-
-        // SEND BACK GUID TO CLIENT 1
-        int n = send(new_socket, &rcv_data, sizeof(rcv_data), 0);
-    }
-
-    /*----------------------------------
-     Accept client 2 connection
-    -----------------------------------*/
-
-    if ((new_socket2 = accept(server_fd, (struct sockaddr *)&server_address, 
-					(socklen_t*)&addrlen)) < 0)
-	{ 
-		perror("accept error 2\n"); 
-		exit(EXIT_FAILURE); 
-	}
-    else{
-        printf("\nTCP Socket 2 Connected!\n");
-
-        /*---------------------------------
-           JOIN & PUBLISH
-        ----------------------------------*/
-        struct ServantData rcv_data;
-
-        // GET OBJ FROM CLIENT 2
-        recv(new_socket2 , &rcv_data, sizeof(rcv_data), 0);
-        rcv_data.GUID = reg.size+1;
-
-        // JOIN
-        reg.servants[reg.size] = rcv_data;
-
-        // PUBLISH
-        publish(reg.size);
-
-        reg.size++;
-
-        // SEND BACK GUID TO CLIENT 2
-        int m = send(new_socket2, &rcv_data, sizeof(rcv_data), 0);
-    }
-
-    /*----------------------------------
-     Join (Register Clients)
-    -----------------------------------*/
-    //join();
-
-    /*----------------------------------
-     Publish 
-    -----------------------------------*/
-    //publish();
-
-    /*-----------------------------------
-     Change socket to non-blocking
-        needs to happen after registration
-    -----------------------------------*/
-    fcntl(new_socket2, F_SETFL, O_NONBLOCK);
-    fcntl(new_socket, F_SETFL, O_NONBLOCK);
-    fcntl(server_fd, F_SETFL, O_NONBLOCK);
-
-    for(;;){
-        printf("\ntcp .\n");
-        // okay i registered the clients, waiting for a message from clients
-        int valread = recv( new_socket , buffer, BUF_SIZE, 0);
-        int valread2 = recv( new_socket2 , buffer, BUF_SIZE, 0);
-
-        // send(new_socket , msg , strlen(msg) , 0);
-        // send(new_socket2 , msg , strlen(msg) , 0);
-        // memset(msg, 0, MSG_LEN);
-        //printf("\nTCP BUFFER\n");
-        printf("%s", buffer);                         //display message
-        bzero(buffer, sizeof(buffer));                //flush buffer
-        sleep(3);                                     //introduce delay or else loops too fast (?)                                  
-    }
-
     pthread_exit(NULL);
 }
 
